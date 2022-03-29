e7dbb9a458e2f548f6474408d186ad5bb0c0e40a
François Nicolas <francois.prm.nicolas@gmail.com>
2022-03-29 09:18:45 +0200

    Essai md

--- FirstMissingPositive.py
+++ FirstMissingPositive.py
@@ -3,42 +3,38 @@ from collections.abc import Callable, MutableSequence
 from numbers import Integral
 
 
-def is_in_range(start: Integral, stop: Integral | None = None, step: Integral = 1):
+def is_in_range(x, start, stop = None, step = 1):
     """
     A naive implementation would be:
     return lambda x: x in range(start, stop, step)
 
-    >>> all(is_in_range(stop)(x) for x, stop in [(5, 8), (0, 5), (3, 4)])
+    >>> all(is_in_range(x, stop) for x, stop in [(5, 8), (0, 5), (3, 4)])
     True
-    >>> any(is_in_range(stop)(x) for x, stop in [(8, 5), (-1, 3), (4, 4), (-10, 0), (-2, - 1)])
+    >>> any(is_in_range(x, stop) for x, stop in [(8, 5), (-1, 3), (4, 4), (-10, 0), (-2, - 1)])
     False
-    >>> all(is_in_range(start, stop)(x) for x, start, stop in [(3, 2, 8), (3, 3, 4), (1, -1, 2)])
+    >>> all(is_in_range(x, start, stop) for x, start, stop in [(3, 2, 8), (3, 3, 4), (1, -1, 2)])
     True
-    >>> any(is_in_range(start, stop)(x) for x, start, stop in [(0, 2, 8), (4, -3, 4), (8, 2, 5)])
+    >>> any(is_in_range(x, start, stop) for x, start, stop in [(0, 2, 8), (4, -3, 4), (8, 2, 5)])
     False
-    >>> all(is_in_range(start, stop, step)(x) for x, start, stop, step in [(9, 1, 15, 4), (0, 8, -3, -2)])
+    >>> all(is_in_range(x, start, stop, step) for x, start, stop, step in [(9, 1, 15, 4), (0, 8, -3, -2)])
     True
-    >>> any(is_in_range(start, stop, step)(x) for x, start, stop, step in [(2, 1, 9, 2), (7, 2, 7, 2), (-6, 1, -10, - 3)])
+    >>> any(is_in_range(x, start, stop, step) for x, start, stop, step in [(2, 1, 9, 2), (7, 2, 7, 2), (-6, 1, -10, - 3)])
     False
     """
     
-    def wrapped(x):
-        if not isinstance(x, Integral):
-            return False
-        q, r = divmod(x - start, step)
-        if r != 0 or q < 0:
-            return False
-        if step > 0:
-            return x < stop
-        return x > stop
-        
     if step == 0: raise ValueError("is_in_range() arg 3 must not be zero")
+    if not isinstance(x, Integral):
+        return False
     if stop is None:
          start, stop = 0, start
-    return wrapped
+    q, r = divmod(x - start, step)
+    if r != 0 or q < 0:
+        return False
+    if step > 0:
+        return x < stop
+    return x > stop
 
-
-def segregate(T: MutableSequence, p: Callable) -> int:
+def segregate(T, p):
     """
     >>> p = lambda x: bool(x)
     >>> T = []; segregate(T, p); T
@@ -82,17 +78,14 @@ def segregate(T: MutableSequence, p: Callable) -> int:
             j -= 1
     return i + bool(p(T[i]))
 
-def FMNN_naive(T: MutableSequence) -> int:
+def FMNN_naive(T):
     """
     The time complexity is quadratic in len(T).
     The space complexity is bounded.
     """    
-    n = 0
-    while n in T:
-        n += 1
-    return n
+    return min(n for n in range(len(T) + 1) if n not in T) 
 
-def FMNN_sort(T: MutableSequence) -> int:
+def FMNN_sort(T):
     """
     The time complexity is linearithmic in len(T).
     The space complexity is that of timsort;
@@ -100,14 +93,14 @@ def FMNN_sort(T: MutableSequence) -> int:
     """
     
     n = len(T)
-    T.sort(key = lambda x: x if is_in_range(n)(x) else n)
+    T.sort(key = lambda x: x if is_in_range(x, n) else n)
     n = 0
     it = iter(T)
     while n in it: n += 1
     return n
 
 
-def FMNN_linear_linear(T: MutableSequence) -> int:
+def FMNN_linear_linear(T):
     """
     The time complexity is linear in len(T).
     The space complexity is also linear in len(T).
@@ -115,15 +108,47 @@ def FMNN_linear_linear(T: MutableSequence) -> int:
     
     is_here = [False] * (len(T) + 1)
     for t in T:
-        if is_in_range(len(T))(t):
+        if is_in_range(t, len(T)):
             is_here[t] = True
     n = 0
     while is_here[n]:
         n += 1
     return n
     
-T = [7, 5, 8, 0, 3, "toto", 1, 15, 2, 5, 2, 0, 1, 5, "aze"]
-print(FMNN_sort(T))
+def FMNN_linear_const(T):
+    """
+    The time complexity is linear in len(T).
+    The space complexity is bounded. 
+    """
+    
+    for i in range(len(T)):
+        assert all(T[j] == j for j in T[:i] if is_in_range(j, len(T))) # Invariant
+        j = T[i]
+        while is_in_range(j, len(T)) and j != T[j]:
+            assert T[i] != i and T[T[i]] != T[i] # Invariant
+            # Swapping T[i] and T[j] ...
+            T[i] = T[j] # The number of fixed points does not decrease 
+            T[j] = j    # The number of fixed points increases by one
+            # ... T[i] and T[j] are swapped.
+            j = T[i]
+    assert all(T[j] == j for j in T if is_in_range(j, len(T))) # Invariant
+    n = 0
+    while n < len(T) and T[n] == n:
+        n += 1
+    return n
+    
+            
+test_set = [[0, 1, 2, 3],
+            [3, 2, 0, 1], 
+            [7, 5, 8, 0, 3, "toto", 1, 15, 2, 5, 2, 0, 1, 5, "aze"], 
+            ]
+
+for T in test_set:
+    print(FMNN_naive(T.copy())
+          == FMNN_sort(T.copy())
+          == FMNN_linear_linear(T.copy())
+          == FMNN_linear_const(T.copy())
+          )
         
 if __name__ == "__main__":
     print("Entering doctest mode !")
